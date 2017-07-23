a2fd3f92b11794eb3dba10b1fec7751b6c5386f3
viveksanthosh <viveksanthosh.in@gmail.com>
2017-07-23 22:36:16 +0530

    added a navnbar component

---
+++ ui/src/components/NavBar.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css'
+export default ({ links }) => (
+  <nav className="navbar navbar-default">
+  <div className="container-fluid">
+    <div className="navbar-header">
+      <a className="navbar-brand" href="#"></a>
+    </div>
+    <ul className="nav navbar-nav">
+     {links.map(link =>  <li><a href={'/'+link}>{Link}</a></li>)}
+    </ul>
+  </div>
+</nav>
+)
