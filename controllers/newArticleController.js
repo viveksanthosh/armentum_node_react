d45c606c122e53a2d0ab39d1d8f483c527675f8d
viveksanthosh <viveksanthosh.in@gmail.com>
2017-07-23 21:38:51 +0530

    removed promisify

--- controllers/newArticleController.js
+++ controllers/newArticleController.js
@@ -2,9 +2,14 @@ const db = require('../database/mongodb'),
     promisify = require("es6-promisify");
 
 module.exports = {
-    getAllNewsArticles: () => {
+    getAllNewsArticles: () => new Promise((resolve, reject) => {
         const collection = db.getInstance().collection('newsArticles'),
-        find = collection.find;
-        return find();
-    }
+            find = collection.find().toArray((err, data) => {
+                if (err) {
+                    reject(err);
+                } else {
+                    resolve(data)
+                }
+            });
+    })
 }
